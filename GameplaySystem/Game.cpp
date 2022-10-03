1e6915f04eca69669d6183ee78b81c0ad4d3bd9d
Valduy <whnpir@gmail.com>
2022-10-03 18:43:52 +0300

    First basic working version

--- GameplaySystem/Game.cpp
+++ GameplaySystem/Game.cpp
@@ -1,11 +1,118 @@
 #include "pch.h"
 #include "Game.h"
 
+#include <fstream>
+#include <iostream>
+#include <mono/jit/jit.h>
+#include <mono/metadata/assembly.h>
+
 namespace engine {
+
+MonoDomain* s_RootDomain;
+MonoDomain* s_AppDomain;
+
+void InitMono()
+{
+	mono_set_assemblies_path("mono/lib");
+
+	MonoDomain* rootDomain = mono_jit_init("MyScriptRuntime");
+	if (rootDomain == nullptr)
+	{
+		// Maybe log some error here
+		return;
+	}
+
+	// Store the root domain pointer
+	s_RootDomain = rootDomain;
+
+	// Create an App Domain
+	s_AppDomain = mono_domain_create_appdomain(const_cast<char*>("MyAppDomain"), nullptr);
+	mono_domain_set(s_AppDomain, true);
+}
+
+char* ReadBytes(const std::string& filepath, uint32_t* outSize)
+{
+	std::ifstream stream(filepath, std::ios::binary | std::ios::ate);
+
+	if (!stream)
+	{
+		// Failed to open the file
+		return nullptr;
+	}
+
+	std::streampos end = stream.tellg();
+	stream.seekg(0, std::ios::beg);
+	uint32_t size = end - stream.tellg();
+
+	if (size == 0)
+	{
+		// File is empty
+		return nullptr;
+	}
+
+	char* buffer = new char[size];
+	stream.read((char*)buffer, size);
+	stream.close();
+
+	*outSize = size;
+	return buffer;
+}
+
+MonoAssembly* LoadCSharpAssembly(const std::string& assemblyPath)
+{
+	uint32_t fileSize = 0;
+	char* fileData = ReadBytes(assemblyPath, &fileSize);
+
+	// NOTE: We can't use this image for anything other than loading the assembly because this image doesn't have a reference to the assembly
+	MonoImageOpenStatus status;
+	MonoImage* image = mono_image_open_from_data_full(fileData, fileSize, 1, &status, 0);
+
+	if (status != MONO_IMAGE_OK)
+	{
+		const char* errorMessage = mono_image_strerror(status);
+		// Log some error message using the errorMessage data
+		return nullptr;
+	}
+
+	MonoAssembly* assembly = mono_assembly_load_from_full(image, assemblyPath.c_str(), &status, 0);
+	mono_image_close(image);
+
+	// Don't forget to free the file data
+	delete[] fileData;
+
+	return assembly;
+}
+
+void PrintAssemblyTypes(MonoAssembly* assembly)
+{
+	MonoImage* image = mono_assembly_get_image(assembly);
+
+	// Get Metadata table of type definitions
+	// All tables definitions - http://docs.go-mono.com/?link=xhtml%3adeploy%2fmono-api-metadata.html
+	const MonoTableInfo* typeDefinitionsTable = mono_image_get_table_info(image, MONO_TABLE_TYPEDEF);
+	int32_t numTypes = mono_table_info_get_rows(typeDefinitionsTable);
+
+	for (int32_t i = 0; i < numTypes; i++)
+	{
+		// Get current row with MONO_TYPEDEF_SIZE columns number
+		uint32_t cols[MONO_TYPEDEF_SIZE];
+		mono_metadata_decode_row(typeDefinitionsTable, i, cols, MONO_TYPEDEF_SIZE);
+
+		// Decode data
+		const char* nameSpace = mono_metadata_string_heap(image, cols[MONO_TYPEDEF_NAMESPACE]);
+		const char* name = mono_metadata_string_heap(image, cols[MONO_TYPEDEF_NAME]);
+
+		printf("%s.%s\n", nameSpace, name);
+	}
+}
 
 void Game::Run() {
 	using namespace std::chrono;
 	using clock = high_resolution_clock;
+
+	InitMono();
+	MonoAssembly* assembly = LoadCSharpAssembly("MonoPlayground.dll");
+	PrintAssemblyTypes(assembly);
 
 	Init();
 
