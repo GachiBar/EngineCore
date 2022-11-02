180404f6225a2fba891df0d63f8cb7a911a52bd3
Valduy <whnpir@gmail.com>
2022-11-03 00:09:32 +0300

    Copy all output content of C# project to working dir

--- Editor/EditorLayer.cpp
+++ Editor/EditorLayer.cpp
@@ -118,21 +118,21 @@ void EditorLayer::OnGuiRender()
 
             if (ImGui::MenuItem("Open...", "Ctrl+O"))
             {
-                std::ifstream ifs("File_1.txt");
+                std::ifstream ifs("..\\File_1.txt");
                 std::string content((std::istreambuf_iterator<char>(ifs)),
                     (std::istreambuf_iterator<char>()));
 
-                (GetApp()->GetEngine()->GetScene())->Deserialize(content);
+                GetApp()->GetEngine()->GetScene()->Deserialize(content);
                 //OpenScene();
             }
 
             if (ImGui::MenuItem("Save", "Ctrl+S"))
             {
                 //SaveScene();
-                std::string json = (GetApp()->GetEngine()->GetScene())->Serialize();
+                std::string json = GetApp()->GetEngine()->GetScene()->Serialize();
                 std::ofstream file_handler;
                 // File Open
-                file_handler.open("File_1.txt");
+                file_handler.open("..\\File_1.txt");
 
                 // Write to the file
                 file_handler << json;
