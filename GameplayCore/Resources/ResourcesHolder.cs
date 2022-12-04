1bfde2951a7de141861347a1ec51f930c5076ee8
Da boi <mihansp1@gmail.com>
2022-12-04 15:47:57 +0300

    Added resource reading optimisation - resource holder that help avoids resource clones(saves memory and syncs data)

---
+++ GameplayCore/Resources/ResourcesHolder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameplayCore.Resources
+{
+    public class ResourcesHolder
+    {
+        private Dictionary<System.Guid, Resource> _resources;
+
+        public ResourcesHolder()
+        {
+            _resources = new Dictionary<Guid, Resource>();
+        }
+
+        public bool TryGet(System.Guid guid, out Resource resource)
+        {
+            if (guid == System.Guid.Empty || !_resources.ContainsKey(guid))
+            {
+                resource = null;
+                return false;
+            }
+
+            resource = _resources[guid];
+            return true;
+        }
+
+        public bool Contains(System.Guid guid) => _resources.ContainsKey(guid);
+
+        public void Set(Resource resource)
+        {
+            _resources[resource.Guid] = resource;
+        }
+    }
+}
